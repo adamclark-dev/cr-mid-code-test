ce9ff09aac44f089d5af8b03ef6356cec7ea91f4
Seb Aebischer <s.aebischer@comicrelief.com>
2023-09-11 13:12:07 +0100

    Fix comment

--- src/Action/Postcode.action.js
+++ src/Action/Postcode.action.js
@@ -6,7 +6,7 @@ import {
 import CONFIGURATION from '../Config/Configuration';
 
 export default LambdaWrapper(CONFIGURATION, async (di, request) => {
-  // Get a name from the query parameters.
+  // get the postcode from the request URL
   const postCode = request.getPathParameter('postCode', null);
 
   return new ResponseModel({
